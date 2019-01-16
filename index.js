94604e76f020b3f0edf46b4da2398d2767b15d5c
bogurs <thsong10@gmail.com>
2019-01-16 17:45:22 +0900

    index(main) encode (POST /api/encode) 구현

--- index.js
+++ index.js
@@ -15,3 +15,34 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
+
+/*
+sample data 
+{
+    count: 111
+    ...
+}
+*/
+// encode json web token with body json data
+app.post('/api/encode', async function(req, res) {
+    console.log('call /api/encode!!!');
+    if (Object.keys(req.body).length === 0) { // if body is null
+        console.log('have to call /api/encode with body json data!!!');
+        return res.status(404).end(); // 404 error return to client
+    }
+    
+    // to Use await, jwt.sign with Promise instance
+    let tokenPromise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve(jwt.sign(req.body, config.secret)))
+    });
+    
+    // await Promise instance and get jwt
+    let token = await tokenPromise;
+
+    /*
+    set token in Request.session
+    { jwt: true }
+    */
+    req.session[token] = true;
+    res.json(token); // return token to client
+});
